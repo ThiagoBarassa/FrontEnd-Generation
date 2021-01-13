d925eb5ce875d9abd958e43e5fce60d0b062edf4
Thiago Barassa <thigobarassa@gmail.com>
2021-01-13 09:07:01 -0300

    adicionando JS basico

---
+++ exercicio1/assets/script.js
@@ -0,0 +1,41 @@
+let nome = document.querySelector('#nome');
+let email = document.querySelector('#email');
+let desc = document.querySelector('#desc');
+
+function validarNome(){
+    let txtNome = document.querySelector('#txtNome')
+    if(nome.value.length < 3){
+        txtNome.innerHTML = 'Nome Invalido'
+        txtNome.style.color = 'red'
+    }
+    else{
+        txtNome.innerHTML = "Nome Valido"
+        txtNome.style.color = 'green' 
+    }
+}
+
+function validarEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if(email.value.indexOf('@')== -1){
+        txtEmail.innerHTML = 'Nome Invalido'
+        txtEmail.style.color = 'red'
+    }
+    else{
+        txtEmail.innerHTML = "Nome Valido"
+        txtEmail.style.color = 'green' 
+    }
+
+}
+function validarDesc(){
+    let txtDesc = document.querySelector('#txtDesc')
+
+    if(desc.value.length > 255){
+        txtDesc.innerHTML = 'Valor de caracteres excedido'
+        txtDesc.style.color = 'red'
+    }
+    else{
+        txtDesc.innerHTML = ""
+        txtDesc.style.color = 'green' 
+    }
+}
