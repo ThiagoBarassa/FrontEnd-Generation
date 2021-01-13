1cdb036cf74084f32a08306aadc4c32e98e4077f
Thiago Barassa <thigobarassa@gmail.com>
2021-01-13 09:52:16 -0300

    mudanças no layout

--- exercicio1/assets/script.js
+++ exercicio1/assets/script.js
@@ -1,6 +1,7 @@
 let nome = document.querySelector('#nome');
 let email = document.querySelector('#email');
 let desc = document.querySelector('#desc');
+let img = document.querySelector('#imgcorpo') 
 
 function validarNome(){
     let txtNome = document.querySelector('#txtNome')
@@ -38,4 +39,12 @@ function validarDesc(){
         txtDesc.innerHTML = ""
         txtDesc.style.color = 'green' 
     }
+}
+function imgZoom(){
+    img.style.width = '600px'
+    img.style.heigth = '450px'
+}
+function imgNormal(){
+    img.style.width = '512px'
+    img.style.heigth = '320px'
 }
